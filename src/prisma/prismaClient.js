c5d31a1a684f579bd1b2a83ca87f252131ea9f91
carreira28 <carreira907@gmail.com>
2026-03-30 13:30:18 +0100

    vercel

--- src/prisma/prismaClient.js
+++ src/prisma/prismaClient.js
@@ -1,13 +1,10 @@
+require("dotenv").config();
+
 const { PrismaClient } = require("@prisma/client");
-const { PrismaPg } = require("@prisma/adapter-pg");
-const { Pool } = require("pg");
+const { withAccelerate } = require("@prisma/extension-accelerate");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate());
 
 module.exports = prisma;
--- src/routes/director.routes.js
+++ src/routes/director.routes.js
@@ -2,16 +2,13 @@ const express = require("express");
 const router = express.Router();
 const directorController = require("../controllers/director.controller");
 
-router.get("/",directorController.getAllDirectors);
+router.get("/", directorController.getAllDirectors);
 
-router.get("/:id",directorController.getDirectorById);
+router.get("/:id/movies", directorController.getDirectorMovies);
 
-router.post("/",directorController.createDirector);
-
-router.put("/:id",directorController.updateDirector);
-
-router.delete("/:id",directorController.deleteDirector);
-
-router.get("/:id/movies",directorController.getDirectorMovies);
+router.get("/:id", directorController.getDirectorById);
+router.post("/", directorController.createDirector);
+router.put("/:id", directorController.updateDirector);
+router.delete("/:id", directorController.deleteDirector);
 
 module.exports = router;
