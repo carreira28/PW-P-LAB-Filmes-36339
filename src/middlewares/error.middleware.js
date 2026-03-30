560f48f7056ff4642fe52f75054feead820fb952
carreira28 <carreira907@gmail.com>
2026-03-30 13:53:09 +0100

    .

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,20 +1,3 @@
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (!token) {
-        return res.status(401).json({ message: "Token não fornecido" });
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({ message: "Token inválido" });
-        }
-        req.user = user;
-        next();
-    });
-};
-
 module.exports = (err, req, res, next) => {
   console.error("[ERRO]", err);
 
